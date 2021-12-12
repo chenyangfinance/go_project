dd757c8cbf74e8ee6458a69579ca69b97ee3f69f
Chen Yang <c.yang@surrey.ac.uk>
2021-12-12 21:01:33 +0000

    Add files via upload

---
+++ test1/main.go
@@ -0,0 +1,13 @@
+package main
+
+//导入语句
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+// go run
+// go build -o '名字'
+
+// go语言一些东西跟Stata很像
---
+++ test2/main.go
@@ -0,0 +1,20 @@
+//变量和常量
+package main
+//go语言变量声明
+
+// var s1 string
+// var age int
+// var isOk bool
+
+var(
+	name string
+	gender bool
+)
+
+func main(){
+	name  ="Tom"
+	gender = true
+
+	fmt.printf("name:%s", name )
+	fmt.println("gender:%s", gender )
+}
